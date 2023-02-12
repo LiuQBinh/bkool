ff1c92e61a13dd10e28fab6e6e3a4af716601926
Lưu Quốc Bình <34769433+LiuQBinh@users.noreply.github.com>
2023-02-12 22:50:39 +0700

    init

--- initial/src/test/LexerSuite.py
+++ initial/src/test/LexerSuite.py
@@ -1,15 +1,22 @@
 import unittest
 from TestUtils import TestLexer
 
+
 class LexerSuite(unittest.TestCase):
-      
     def test_lowercase_identifier(self):
         """test identifiers"""
-        self.assertTrue(TestLexer.test("abc","abc,<EOF>",101))
+        print('test_lowercase_identifier')
+        self.assertTrue(TestLexer.test("abc", "abc,<EOF>", 101))
+
     def test_lower_upper_id(self):
-        self.assertTrue(TestLexer.test("aCBbdc","aCBbdc,<EOF>",102))
+        print('test_lower_upper_id')
+        self.assertTrue(TestLexer.test("aCBbdc", "aCBbdc,<EOF>", 102))
+
     def test_mixed_id(self):
-        self.assertTrue(TestLexer.test("aAsVN3","aAsVN,3,<EOF>",103))
+        print('test_mixed_id')
+        self.assertTrue(TestLexer.test("aAsVN3", "aAsVN,3,<EOF>", 103))
+
     def test_integer(self):
         """test integers"""
-        self.assertTrue(TestLexer.test("123a123","123,a,123,<EOF>",104))
+        print('test_integer')
+        self.assertTrue(TestLexer.test("123a123", "123,a,123,<EOF>", 104))
